75aa41bfa43e8fb623b3333c0d7ebf4f2944a4fc
Sreetam Das <sreetamdas@gmail.com>
2017-12-15 22:36:34 +0530

    Dynamic image hosting using Drive export

--- src/components/Members.js
+++ src/components/Members.js
@@ -16,10 +16,15 @@ class Members extends React.Component {
 		const {details} = this.props;
 		console.log(details);
 
-		const empty = details.length===0 ? true : false
+		const empty = details.length===0 ? true : false;
 
 		console.log(empty);
 		// console.log(JSON.stringify(this.props.details));
+		// https://drive.google.com/open?id=1oN1EbU9Y1-2Xpsjwv0ieFONqDXyTMoVp
+		// https://avatars3.githubusercontent.com/u/11270438?s=400&u=1664aa962a38ed0ae257b153dffb37891581c80a&v=4
+
+		const image_url_string = new URL('https://drive.google.com/open?id=1oN1EbU9Y1-2Xpsjwv0ieFONqDXyTMoVp'),
+			image_id = image_url_string.searchParams.get("id");
 
 		return(
 			<div>
@@ -27,7 +32,7 @@ class Members extends React.Component {
 				<div className="container-fluid">
 					<div className="row">
 						<div className="col-lg-12 text-center">
-							<img src="https://avatars3.githubusercontent.com/u/11270438?s=400&u=1664aa962a38ed0ae257b153dffb37891581c80a&v=4" height="200px" className="rounded-circle" alt="Sreetam Das" />
+							<img src={`https://drive.google.com/uc?export=view&id=${image_id}`} height="200px" className="rounded-circle" alt={details.name} />
 							<p className="americanCaptain noUnderline">
 								<a style={nameStyle}>
 									{details.name}
