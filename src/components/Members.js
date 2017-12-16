b8b18c163ddc6fb2d055fea6f229182a55dc9830
Sreetam Das <sreetamdas@gmail.com>
2017-12-16 21:23:00 +0530

    Render individual members now, WORKS

--- src/components/Members.js
+++ src/components/Members.js
@@ -2,54 +2,62 @@ import React from "react";
 // import Individual from './Individual';
 
 class Members extends React.Component {
-	render() {
-		var nameStyle = {
+	render_member(member, key) {
+		const nameStyle = {
 			fontSize: "3em",
-		};
-
-		var quoteStyle = {
-			fontSize: "0.7em",
+			color: "black",
+			// fontWeight: 700,
+		},
+			quoteStyle = {
+			fontSize: "1.7em",
 			paddingTop: 0,
-		};
+			color: "black",
+			// fontWeight: 700,
+		};	
+		console.log(nameStyle);
+		let image_url_string = new URL(member.image),
+			image_id = image_url_string.searchParams.get("id");
 
-		const { details } = this.props;
-		console.log(details);
+		return (
+			<div className="col-lg-4 text-center" key={key}>
+				<img
+					src={`https://drive.google.com/uc?export=view&id=${image_id}`}
+					height="200px"
+					className="rounded-circle"
+					alt={member.name}
+				/>
+				<p className="americanCaptain noUnderline">
+					<a style={nameStyle}>{member.name}</a>
+					<br />
+					<span className="precursive" style={quoteStyle}>
+						{member.quote}
+					</span>
+				</p>
+			</div>
+		);
+	}
 
-		const empty = details.length === 0 ? true : false;
+	render() {
+		const member_list = this.props.list;
+		// console.log(details);
 
-		console.log(empty);
+		const empty = member_list.length === 0 ? true : false;
+
+		// console.log(empty);
 		// console.log(JSON.stringify(this.props.details));
 		// https://drive.google.com/open?id=1oN1EbU9Y1-2Xpsjwv0ieFONqDXyTMoVp
 		// https://avatars3.githubusercontent.com/u/11270438?s=400&u=1664aa962a38ed0ae257b153dffb37891581c80a&v=4
-
-		const image_url_string = new URL(
-				"https://drive.google.com/open?id=1oN1EbU9Y1-2Xpsjwv0ieFONqDXyTMoVp",
-			),
-			image_id = image_url_string.searchParams.get("id");
 
 		return (
 			<div>
 				{!empty ? (
 					<div className="container-fluid">
 						<div className="row">
-							<div className="col-lg-12 text-center">
-								<img
-									src={`https://drive.google.com/uc?export=view&id=${image_id}`}
-									height="200px"
-									className="rounded-circle"
-									alt={details.name}
-								/>
-								<p className="americanCaptain noUnderline">
-									<a style={nameStyle}>{details.name}</a>
-									<br />
-									<span
-										className="precursive"
-										style={quoteStyle}
-									>
-										{details.quote}
-									</span>
-								</p>
-							</div>
+						{/* {console.log(member_list)} */}
+						{/* {console.log("member list: ", member_list)} */}
+							{Object.keys(member_list).map(key => (
+								this.render_member(member_list[key], key)
+							))}
 						</div>
 					</div>
 				) : (
