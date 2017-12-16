3cf4509e2ce688ae5d31a44bb15cce4aa4a74b53
Sreetam Das <sreetamdas@gmail.com>
2017-12-16 21:36:36 +0530

    Clean up Members, Add Kasala as Deafult Image

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,21 +1,20 @@
 import React from "react";
-// import Individual from './Individual';
 
 class Members extends React.Component {
 	render_member(member, key) {
 		const nameStyle = {
 			fontSize: "3em",
 			color: "black",
-			// fontWeight: 700,
 		},
 			quoteStyle = {
 			fontSize: "1.7em",
 			paddingTop: 0,
 			color: "black",
-			// fontWeight: 700,
 		};	
 		console.log(nameStyle);
-		let image_url_string = new URL(member.image),
+		let image_url_string = new URL(typeof member.image === "undefined"
+										? "https://drive.google.com/open?id=1QwSSN4kXEERbydtgpomfuqw9-RSw4PfH" // Kasala is default image obviously
+										: member.image),
 			image_id = image_url_string.searchParams.get("id");
 
 		return (
@@ -39,29 +38,20 @@ class Members extends React.Component {
 
 	render() {
 		const member_list = this.props.list;
-		// console.log(details);
-
 		const empty = member_list.length === 0 ? true : false;
-
-		// console.log(empty);
-		// console.log(JSON.stringify(this.props.details));
-		// https://drive.google.com/open?id=1oN1EbU9Y1-2Xpsjwv0ieFONqDXyTMoVp
-		// https://avatars3.githubusercontent.com/u/11270438?s=400&u=1664aa962a38ed0ae257b153dffb37891581c80a&v=4
 
 		return (
 			<div>
 				{!empty ? (
 					<div className="container-fluid">
 						<div className="row">
-						{/* {console.log(member_list)} */}
-						{/* {console.log("member list: ", member_list)} */}
 							{Object.keys(member_list).map(key => (
 								this.render_member(member_list[key], key)
 							))}
 						</div>
 					</div>
 				) : (
-					<div>Nothing yet</div>
+					<h3>Nothing yet</h3>
 				)}
 			</div>
 		);
