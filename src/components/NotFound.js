c2f1054bdbe99f68ebc8c617351d5e8614f75f81
Sreetam Das <sreetamdas@gmail.com>
2017-12-17 16:23:36 +0530

    Update the 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class NotFound extends React.Component {
 	render() {
-		return <h1>Error 404!</h1>;
+		const styles_404 = {
+				fontSize: "15vh",
+			};
+			// styles_message = {
+			// 	color: 
+			// }
+		return (
+			<div className="full-page">
+				<h1
+					className="white-text vertical-center josefinSlab"
+					style={styles_404}
+				>
+					Error 404!
+					<br/>
+					<span style={{ fontSize: "6vh"}}>The page you are looking for does not exist :(</span>
+				</h1>
+			</div>
+		);
 	}
 }
 
