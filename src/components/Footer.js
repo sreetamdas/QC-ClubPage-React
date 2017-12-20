77973da66e48704cd651cbad7e87aa12209c062d
Sreetam Das <sreetamdas@gmail.com>
2017-12-20 10:52:26 +0530

    Prettier Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,25 @@
 import React from "react";
-import FontAwesomeIcon from "@fortawesome/react-fontawesome"
-import { faCoffee, faHeart, faCode, faPhone, faEnvelope, faRss } from "@fortawesome/fontawesome-free-solid"
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import {
+	faCoffee,
+	faHeart,
+	faCode,
+	faPhone,
+	faEnvelope,
+	faRss,
+} from "@fortawesome/fontawesome-free-solid";
 
 const Footer = () => (
 	<section>
 		<div className="row josefinSlab fa-icons">
-			<div className="col-lg-12 text-center" style={{ paddingBottom: "40px" }}>
-				<h2	className="section-heading ailerons" style={{ fontSize: "40px" }}>
+			<div
+				className="col-lg-12 text-center"
+				style={{ paddingBottom: "40px" }}
+			>
+				<h2
+					className="section-heading ailerons"
+					style={{ fontSize: "40px" }}
+				>
 					Let's Get In Touch!
 				</h2>
 				<p className="orange" style={{ fontSize: "30px" }}>
@@ -15,14 +28,18 @@ const Footer = () => (
 				<hr className="primary" />
 			</div>
 			<div className="col-lg-2 offset-lg-2 text-center">
-				<FontAwesomeIcon icon={ faPhone } className="fa-3x" flip="horizontal"/>
+				<FontAwesomeIcon
+					icon={faPhone}
+					className="fa-3x"
+					flip="horizontal"
+				/>
 				<p style={{ fontSize: "28px", fontWeight: "500" }}>
 					+91 9701 832 369
 				</p>
 			</div>
 			<div className="col-lg-4 text-center noUnderline">
 				<a href="https://www.facebook.com/qcnitw">
-					<FontAwesomeIcon icon={ faRss } className="fa-3x" />
+					<FontAwesomeIcon icon={faRss} className="fa-3x" />
 					<p style={{ fontSize: "28px", fontWeight: "500" }}>
 						Follow us for Updates!
 					</p>
@@ -30,18 +47,31 @@ const Footer = () => (
 			</div>
 			<div className="col-lg-2 text-center noUnderline">
 				<a href="mailto:quizclub@student.nitw.ac.in">
-					<FontAwesomeIcon icon={ faEnvelope } className="fa-3x" />
+					<FontAwesomeIcon icon={faEnvelope} className="fa-3x" />
 					<p style={{ fontSize: "28px", fontWeight: "500" }}>
 						Mail Us!
 					</p>
 				</a>
 			</div>
 		</div>
-		<div className="row ailerons orange text-center" style={{ paddingTop: "100px" }}>
-			<div style={{ borderRight: "3px solid", height: "40px" }} className="col-12">
-				<p	style={{ fontSize: "28px" }} id="copyright">
-					<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">
-						<img alt="Creative Commons License" style={{ borderWidth: 0 }} src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" />
+		<div
+			className="row ailerons orange text-center"
+			style={{ paddingTop: "100px" }}
+		>
+			<div
+				style={{ borderRight: "3px solid", height: "40px" }}
+				className="col-12"
+			>
+				<p style={{ fontSize: "28px" }} id="copyright">
+					<a
+						rel="license"
+						href="http://creativecommons.org/licenses/by-nc/4.0/"
+					>
+						<img
+							alt="Creative Commons License"
+							style={{ borderWidth: 0 }}
+							src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"
+						/>
 					</a>{" "}
 					Quiz Club, NIT Warangal
 				</p>
@@ -50,11 +80,15 @@ const Footer = () => (
 				<p style={{ fontSize: "28px" }}>
 					Made with{" "}
 					<a href="">
-						<FontAwesomeIcon icon={ faHeart }/>
+						<FontAwesomeIcon icon={faHeart} />
 					</a>
-					, <FontAwesomeIcon icon={ faCoffee}/> and{" "}
-					<FontAwesomeIcon icon={ faCode }/> by{" "}
-					<a id="credits" href="https://bit.do/sreetamdas" style={{ border: "2px solid" }}>
+					, <FontAwesomeIcon icon={faCoffee} /> and{" "}
+					<FontAwesomeIcon icon={faCode} /> by{" "}
+					<a
+						id="credits"
+						href="https://bit.do/sreetamdas"
+						style={{ border: "2px solid" }}
+					>
 						Sreetam Das
 					</a>
 				</p>
