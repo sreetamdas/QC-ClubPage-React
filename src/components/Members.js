d2a4fa9b033a4a0334452f984f2f5e91754b6029
Sreetam Das <sreetamdas@gmail.com>
2017-12-23 02:26:39 +0530

    Respect image ID if local

--- src/components/Members.js
+++ src/components/Members.js
@@ -3,29 +3,38 @@ import React from "react";
 class Members extends React.Component {
 	render_member(member, key) {
 		const nameStyle = {
-			fontSize: "3em",
-		},
+				fontSize: "3em",
+			},
 			nameClasses = "josefinSlab noUnderline white-text",
 			quoteStyle = {
-			fontSize: "1.7em",
-			paddingTop: 0,
-		},
-			quoteClasses = "josefinSlab orange";	
-		let image_url_string = new URL(typeof member.image === "undefined"
-										? "https://drive.google.com/open?id=1QwSSN4kXEERbydtgpomfuqw9-RSw4PfH" // Kasala is default image obviously
-										: member.image),
-			image_id = image_url_string.searchParams.get("id");
-
+				fontSize: "1.7em",
+				paddingTop: 0,
+			},
+			quoteClasses = "josefinSlab orange";
+		console.log("original:", member.image);
+		// console.log(process.env.PUBLIC_URL);
+		let local_image = false,
+			image_id =
+				typeof member.image === "undefined"
+					? "https://drive.google.com/open?id=1QwSSN4kXEERbydtgpomfuqw9-RSw4PfH" // Kasala is default image obviously
+					: member.image.substring(0, 5) === "/img/"
+						? local_image = true
+						: `https://drive.google.com/uc?export=view&id=${new URL(
+								member.image,
+							).searchParams.get("id")}`;
+		console.log("imageid:", image_id);
 		return (
 			<div className="col text-center" key={key}>
 				<img
-					src={`https://drive.google.com/uc?export=view&id=${image_id}`}
+					src={local_image ? process.env.PUBLIC_URL + member.image : image_id}
 					height="200px"
 					className="rounded-circle"
 					alt={member.name}
 				/>
 				<p>
-					<a style={nameStyle} className={nameClasses}>{member.name}</a>
+					<a style={nameStyle} className={nameClasses}>
+						{member.name}
+					</a>
 					<br />
 					<span className={quoteClasses} style={quoteStyle}>
 						{member.quote}
@@ -44,9 +53,9 @@ class Members extends React.Component {
 				{!empty ? (
 					<div className="container-fluid">
 						<div className="row">
-							{Object.keys(member_list).map(key => (
-								this.render_member(member_list[key], key)
-							))}
+							{Object.keys(member_list).map(key =>
+								this.render_member(member_list[key], key),
+							)}
 						</div>
 					</div>
 				) : (
