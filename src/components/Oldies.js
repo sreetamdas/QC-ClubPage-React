3729d93b213f6da70c8d82ef18002340b457f96c
Sreetam Das <sreetamdas@gmail.com>
2018-01-08 22:44:00 +0530

    Oldies added

--- src/components/Oldies.js
+++ src/components/Oldies.js
@@ -4,18 +4,21 @@ import base from "./base";
 import Loader from "./Loader";
 import Header from "./Header";
 import Footer from "./Footer";
-// import LoadingBar from "react-loadingbar";
+import axios from "axios";
 
 class Oldies extends React.Component {
 	constructor() {
 		super();
 
-		this.add_some = this.add_some.bind(this);
+		this.addMember = this.addMember.bind(this);
 
 		this.state = {
-			exMembers: {},
+			exMembers: {
+				"2016": [],
+				"2017": [],
+			},
 			loaded: false,
-			progress: 30,
+			devOptions: false,
 		};
 	}
 
@@ -36,47 +39,106 @@ class Oldies extends React.Component {
 		base.removeBinding(this.ref);
 	}
 
-	sortMembers() {
-		const order = [
-				"gensec",
-				"finalYears",
-				"thirdYears",
-				"secondYears",
-				"firstYears",
-			],
-			sorted_club = {};
-
-		for (let i = 0; i < order.length; i++) {
-			const element = order[i];
-			Object.keys(this.state.clubMembers).forEach(key => {
-				if (key === element) {
-					sorted_club[key] = this.state.clubMembers[key];
-				}
-			});
-		}
-		return sorted_club;
-	}
-
-	add_some() {
-		const member = {
-			some: "thing",
-		};
+	addMember(member) {
+		let exMembers = { ...this.state.exMembers };
+		Array.isArray(member)
+			? Object.keys(member).map(
+					key =>
+						typeof member[key].year !== "undefined"
+							? exMembers[`${member[key].year}`].push(member[key])
+							: console.log("not pushing", key),
+				)
+			: exMembers[`${member.year}`].push(member);
 		this.setState({
-			exMembers: member,
+			exMembers: exMembers,
 		});
 	}
 
+	get_from_google_spreadsheet() {
+		this.setState({
+			loaded: false,
+		});
+
+		const api_key = "AIzaSyB5FLnTEzfV-YrVPf7eUNFkQu9h9VJmGK4",
+			sheet_id = "1YXziLAuUY4-PBBsTyXqHnl_ja3zB4OQbAybByvAxj_4",
+			range = "A2:F420",
+			url = `https://sheets.googleapis.com/v4/spreadsheets/${sheet_id}/values/Sheet3!${range}`;
+
+		const final = `${url}?key=${api_key}`;
+		console.log(`request at ${final}`);
+
+		axios
+			.get(url, {
+				params: {
+					key: api_key,
+				},
+			})
+			.then(response => {
+				// call function here
+				this.setState({
+					loaded: true,
+				});
+				console.log(response);
+
+				this.process_google_spreadsheet(response);
+			})
+			.catch(error => {
+				this.setState({
+					clicked_get_from_sheet: true,
+				});
+				alert(error, "asdasdasdasd");
+			});
+	}
+	process_google_spreadsheet(sheet) {
+		let sheet_data = sheet.data.values; // this is an array
+		let new_members = [];
+
+		Object.keys(sheet_data).forEach(key => {
+			let new_member = {}; // if higher scope then changes all attached values ???
+
+			Object.keys(sheet_data[key]).forEach(i => {
+				switch (i) {
+					case `1`:
+						new_member[`name`] = sheet_data[key][i];
+						break;
+					case `2`:
+						new_member[`quote`] = sheet_data[key][i];
+						break;
+					case `3`:
+						new_member[`image`] = sheet_data[key][i];
+						break;
+					case `4`:
+						new_member[`year`] = sheet_data[key][i];
+						break;
+					default:
+						break;
+				}
+			});
+			new_members.push(new_member);
+		});
+		this.addMember(new_members);
+	}
+
 	render() {
-		this.add_some();
 		return (
 			<div className="black-bg">
-				{this.state.loaded ? <Header /> : null}
+				<button
+					className="btn btn-outline-primary"
+					onClick={() => this.get_from_google_spreadsheet()}
+					style={!this.state.devOptions ? { display: "none" } : {}}
+				>
+					Get
+				</button>
+				{this.state.loaded ? (
+					<Header heading={"Quiz Club Oldies"} />
+				) : null}
 				{this.state.loaded ? (
 					Object.keys(this.state.exMembers).map(
 						key => (
 							<Members
 								key={key}
 								list={this.state.exMembers[key]}
+								heading={`Batch of ${key}`}
 							/>
 						), // Passes an entire batch at a time
 					)
@@ -84,7 +146,6 @@ class Oldies extends React.Component {
 					<Loader message="Loading" />
 				)}
 				{this.state.loaded ? <Footer /> : null}
-				{/* <Loader message="Loading" /> */}
 			</div>
 		);
 	}
